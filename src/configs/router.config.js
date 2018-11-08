141909f02afcf63561ec8184b0fec7318d81075f
Arison <784602719@qq.com>
2018-11-08 18:04:30 +0800

    完成作业发布功能

--- src/configs/router.config.js
+++ src/configs/router.config.js
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import AppHomePage from "../modules/home/AppHomePage";
 import BindMenu from "../modules/accountBind/BindMenu";
 import AccountBind from "../modules/accountBind/AccountBind";
+import ReleaseAssignmentPage from "../modules/homework/ReleaseAssignmentPage";
+import AssignmentListPage from "../modules/homework/AssignmentListPage";
+import AssignmentDetailPage from "../modules/homework/AssignmentDetailPage";
 
 export default class RouteConfig extends Component {
 
@@ -10,7 +13,7 @@ export default class RouteConfig extends Component {
         return (
             <Router>
                 <div>
-                    <Route exact path="/" component={AppHomePage}/>
+                    <Route exact path="/" component={AssignmentDetailPage}/>
                     {/*<Route path='/bindMenu' component={BindMenu}/>*/}
                     {/*<Route path='/accountBind/:type?' component={AccountBind}/>*/}
                 </div>
--- src/modules/homework/AssignmentListPage.js
+++ src/modules/homework/AssignmentListPage.js
@@ -4,16 +4,49 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './AssignmentListPage.css'
+import { List} from 'antd';
 /**
  * 作业列表
  * Created by Arison on 17:48.
  */
+const data = [
+    {
+        title: '语文作业',
+        state:'未读',
+        content:'背诵课文',
+        end_time:'2018-08-23 16:00',
+        publisher:'陈莉莉'
+    },
+    {
+        title: '数学作业',
+        state:'未读',
+        content:'背诵课文',
+        end_time:'2018-08-23 16:00',
+        publisher:'陈莉莉'
+    },
+    {
+        title: '英语作业',
+        state:'未读',
+        content:'背诵课文',
+        end_time:'2018-08-23 16:00',
+        publisher:'陈莉莉'
+    },
+    {
+        title: '物理作业',
+        state:'未读',
+        content:'背诵课文',
+        end_time:'2018-08-23 16:00',
+        publisher:'陈莉莉'
+    }
+];
 class AssignmentListPage extends React.Component{
     constructor(props){
         super(props);
         this.state={
             name:'AssignmentListPage'
         };
+
     }
 
 
@@ -22,8 +55,36 @@ class AssignmentListPage extends React.Component{
     }
 
     render(){
-        return <div>
-
+        return <div className="container-fluid" style={{padding:"0px",height:"1000px",backgroundColor:"#F3F3F3"}}>
+            <List
+                id="assignment_list"
+                dataSource={data}
+                renderItem={item => (
+                    <List.Item   className="row"  id="list_item_noBorder">
+                            <div className="col-xs-12" >
+                                   <div className="row" id="padding">
+                                       <div className="col-xs-6" id="row_left">
+                                           <span id="span_header_left">{item.title}</span></div>
+                                       <div className="col-xs-6" id="row_right">
+                                           <span id="span_header_right">{item.state}</span></div>
+                                   </div>
+                                <div id="page_horizontal_line"></div>
+                                <div className="row" id="padding">
+                                    <div className="col-xs-4">内容：</div>
+                                    <div className="col-xs-8"><span id="span_display">{item.content}</span></div>
+                                </div>
+                                <div className="row" id="padding">
+                                    <div className="col-xs-4">截止时间：</div>
+                                    <div className="col-xs-8"><span id="span_display">{item.end_time}</span></div>
+                                </div>
+                                <div className="row" id="padding">
+                                    <div className="col-xs-4">发布老师：</div>
+                                    <div className="col-xs-8"><span id="span_display">{item.publisher}</span></div>
+                                </div>
+                         </div>
+                    </List.Item>
+                )}
+            />
         </div>
     }
 }

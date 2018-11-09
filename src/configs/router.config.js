700b8b882d9a585d4b81a61d83320ab7fda2042f
Arison <784602719@qq.com>
2018-11-09 18:07:11 +0800

    Merge branch 'master' of https://github.com/Arisono/smart-school
    # Conflicts:
    #	src/configs/router.config.js

--- src/configs/router.config.js
+++ src/configs/router.config.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import AppHomePage from "../modules/home/AppHomePage";
 import BindMenu from "../modules/accountBind/BindMenu";
 import AccountBind from "../modules/accountBind/AccountBind";
-import ReleaseAssignmentPage from "../modules/homework/ReleaseAssignmentPage";
-import AssignmentListPage from "../modules/homework/AssignmentListPage";
-import AssignmentDetailPage from "../modules/homework/AssignmentDetailPage";
-import LeaveListPage from "../modules/leave/LeaveListPage";
+import NewAlbum from "../modules/album/NewAlbum";
+import UploadImage from "../modules/album/UploadImage";
+import UploadVideo from "../modules/video/UploadVideo";
+import ClassAlbum from "../modules/album/ClassAlbum";
+import PictureList from "../modules/album/PictureList";
+import VideoPlayer from "../modules/video/VideoPlayer";
+import PrincipalMailbox from "../modules/principalMailbox/PrincipalMailbox";
+import MeetingSignIn from "../modules/meeting/MeetingSignIn";
+import PhonesSelect from "../modules/phonesBook/PhonesSelect";
+import PhonesList from "../modules/phonesBook/PhonesList";
 
 export default class RouteConfig extends Component {
 
@@ -14,9 +20,28 @@ export default class RouteConfig extends Component {
         return (
             <Router>
                 <div>
-                    <Route exact path="/" component={LeaveListPage}/>
-                    {/*<Route path='/bindMenu' component={BindMenu}/>*/}
-                    {/*<Route path='/accountBind/:type?' component={AccountBind}/>*/}
+                    <Route exact path="/" component={AppHomePage}/>
+
+                    {/*饶猛*/}
+                    <Route path='/bindMenu' component={BindMenu}/>
+                    <Route path='/accountBind/:type?' component={AccountBind}/>
+                    <Route path='/newAlbum' component={NewAlbum}/>
+                    <Route path='/uploadImage' component={UploadImage}/>
+                    <Route path='/uploadVideo' component={UploadVideo}/>
+                    <Route path='/classAlbum' component={ClassAlbum}/>
+                    <Route path='/pictureList/:title?' component={PictureList}/>
+                    {/*<Redirect exact from='/pictureList' to={{pathname: '/pictureList/相册'}}/>*/}
+                    <Route path='/videoPlayer/:title?' component={VideoPlayer}/>
+                    <Route path='/principalMailbox' component={PrincipalMailbox}/>
+                    <Route path='/meetingSignIn' component={MeetingSignIn}/>
+                    <Route path='/phonesSelect' component={PhonesSelect}/>
+                    <Route path='/phonesList/:classTitle?' component={PhonesList}/>
+
+                    {/*刘杰*/}
+
+                    {/*方龙海*/}
+
+                    {/*刘金龙*/}
                 </div>
             </Router>
         );

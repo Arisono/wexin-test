3d0d0155c1489fc24149488d80a4470489b68d43
time <meforlong@163.com>
2018-11-21 15:41:45 +0800

    1.通知公告下拉刷新

--- src/modules/consumeManager/ConsumeRePage.jsx
+++ src/modules/consumeManager/ConsumeRePage.jsx
@@ -1,10 +1,12 @@
-import React, {Component} from 'react'
-import ConsumeBean from 'model/ConsumeBean'
-import {List} from 'antd'
+import React, {Component} from 'react';
+import ConsumeBean from 'model/ConsumeBean';
+import {List, Skeleton} from 'antd';
 import ConsumeReItem from "../../components/ConsumeReItem";
-import 'css/consume-re.css'
+import 'css/consume-re.css';
 import {isObjEmpty} from "../../utils/common";
-
+import {Toast} from "antd-mobile";
+import InfiniteScroll from "react-infinite-scroller";
+import LoadingMore from 'components/LoadingMore'
 export default class ConsumeRePage extends Component {
 
     constructor() {
@@ -12,13 +14,15 @@ export default class ConsumeRePage extends Component {
 
         this.state = {
             consumeList: [],
-            typeTitle: ''
+            typeTitle: '',
+            hasMoreData: true,
+            isLoading: true
         }
     }
 
     componentDidMount() {
         document.title = '消费记录'
-
+        Toast.loading('努力加载中...', 0)
         for (let i = 0; i < 20; i++) {
             let consumeBean = new ConsumeBean()
             consumeBean.chargeName = '线上充值'+i
@@ -34,17 +38,25 @@ export default class ConsumeRePage extends Component {
     }
 
     render() {
-        const {consumeList, typeTitle} = this.state
+        const {consumeList, typeTitle,hasMoreData, isLoading} = this.state
 
         return (
             <div className='consume-select-root'>
                 <div className={isObjEmpty(typeTitle) ? 'displayNone' : 'consume-list-header'}>{typeTitle}</div>
                 <div className={isObjEmpty(typeTitle) ? 'displayNone' : 'gray-line'}></div>
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={this.loadRechargeList}
+                    hasMore={hasMoreData}
+                    loader={<LoadingMore/>}>
+                    <Skeleton loading={isLoading} active paragraph={{rows: 3}}>
                 <List className='phones-list-layout' dataSource={consumeList} renderItem={consumeBean => (
                     <List.Item>
                         <ConsumeReItem consumeBean={consumeBean}/>
                     </List.Item>
-                )}/>
+                    )}/>
+                    </Skeleton>
+                </InfiniteScroll>
             </div>
         )
     }}

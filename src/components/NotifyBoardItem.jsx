1f395639e72b26637f7a3d87d7202609653172a5
time <meforlong@163.com>
2018-11-22 16:35:01 +0800

    样式调整

--- src/components/NotifyBoardItem.jsx
+++ src/components/NotifyBoardItem.jsx
@@ -24,10 +24,11 @@ export default class NotifyBoardItem extends Component {
         return (
             <div className='notify-item-root'>
                 <div className='notify-item-bg'>
-                <div className = 'notify-item-top'>
-                <div className='notify-item-title'>{notifyBoBean.noTitle}</div>
-                    <div className={notifyBoBean.noStatu=='已读' ? 'notify-item-statuAl':'notify-item-statuNo'}>{notifyBoBean.noStatu}</div>
-                </div>
+                    <div className='notify-item-top'>
+                        <div className='notify-item-title'>{notifyBoBean.noTitle}</div>
+                        <div
+                            className={notifyBoBean.noStatu == '已读' ? 'notify-item-statuAl' : 'notify-item-statuNo'}>{notifyBoBean.noStatu}</div>
+                    </div>
                     <div></div>
                     <div className='notify-item-line'></div>
                     <div className="notify-item-isserLine">
@@ -38,16 +39,16 @@ export default class NotifyBoardItem extends Component {
 
                     <div className="notify-item-timeLine">
                         <div className='notify-item-time'>通知时间：</div>
-                    <div className='notify-item-timeDetail'>{notifyBoBean.noTime}</div>
+                        <div className='notify-item-timeDetail'>{notifyBoBean.noTime}</div>
                     </div>
                     <div className='notify-item-lineBottem'></div>
                     <div className="notify-item-detailLine">
                         <div className="notify-item-detail">详情</div>
-                        <img className='notify-item-detailIcon' src={require('imgs/next_arrow.png')} />
+                        <img className='notify-item-detailIcon' src={require('imgs/next_arrow.png')}/>
                         {/*<img className='notify-item-detailIcon' src={require('imgs/next_arrow.png')} />*/}
                     </div>
 
-            </div>
+                </div>
             </div>
         )
     }

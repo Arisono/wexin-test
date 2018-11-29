49e0e1f30679bf6663918b88495035b0dd061e19
RaoMeng <raomeng0109@163.com>
2018-11-29 15:09:04 +0800

    相册更新接口调整

--- src/modules/video/WonderMoment.jsx
+++ src/modules/video/WonderMoment.jsx
@@ -4,11 +4,12 @@
  */
 
 import React, {Component} from 'react'
-import {isObjEmpty} from "../../utils/common"
 import {Icon, List} from 'antd'
-import {Picker, List as Mlist} from 'antd-mobile'
-import ReactPlayer from 'react-player'
+import {Picker, List as Mlist, Toast} from 'antd-mobile'
 import VideoItem from 'components/VideoItem'
+import {fetchGet} from "../../utils/fetchRequest";
+import {API} from "../../configs/api.config";
+import ClassBean from 'model/ClassBean'
 
 export default class WonderMoment extends Component {
 
@@ -17,7 +18,7 @@ export default class WonderMoment extends Component {
 
         this.state = {
             classList: [],
-            classText: '',
+            classValue: [],
             videoList: []
         }
     }
@@ -31,21 +32,10 @@ export default class WonderMoment extends Component {
     componentDidMount() {
         document.title = '精彩瞬间'
 
-        const {classList, videoList} = this.state
+        Toast.loading('', 0)
+        this.getClassList()
 
-        for (let i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                classList.push({
-                    label: '三年级（一）班',
-                    value: '三年级（一）班'
-                })
-            } else {
-                classList.push({
-                    label: '三年级（二）班',
-                    value: '三年级（二）班'
-                })
-            }
-        }
+        const {classList, videoList} = this.state
 
         for (let i = 0; i < 10; i++) {
             videoList.push({
@@ -57,7 +47,7 @@ export default class WonderMoment extends Component {
     }
 
     render() {
-        const {classList, classText, videoList} = this.state
+        const {classList, classValue, videoList} = this.state
 
         let addIcon = ''
 
@@ -72,7 +62,7 @@ export default class WonderMoment extends Component {
             <div style={{width: '100%', height: '100vh', display: 'flex', flexDirection: 'column'}}>
                 <div className='gray-line'></div>
                 <Picker data={classList} title='选择班级' extra='请选择'
-                        value={classText} onChange={this.handleClassChange}
+                        value={classValue} onChange={this.handleClassChange}
                         onOk={this.handleClassChange} cols={1}>
                     <Mlist.Item arrow="horizontal">选择班级</Mlist.Item>
                 </Picker>
@@ -91,6 +81,62 @@ export default class WonderMoment extends Component {
         )
     }
 
+    getClassList = () => {
+        const {classList, classValue} = this.state
+        classList.length = 0
+
+        fetchGet(API.GET_CLASS_LIST, {
+            userId: 10002,
+        }).then(response => {
+            Toast.hide()
+
+            this.analysisClassList(response)
+        }).catch(error => {
+            Toast.hide()
+            if (typeof error === 'string') {
+                Toast.fail(error, 2)
+            }
+        })
+    }
+
+    analysisClassList = response => {
+        const {classList, classValue} = this.state
+        classList.length = 0
+        classValue.length = 0
+
+        let dataArray = response.data
+        if (dataArray) {
+            for (let i = 0; i < dataArray.length; i++) {
+                let dataObject = dataArray[i]
+                if (dataObject) {
+                    let classBean = new ClassBean()
+
+                    classBean.label = dataObject.parentName + dataObject.schName
+                    classBean.value = i
+                    classBean.schId = dataObject.schId
+                    classBean.parentId = dataObject.parentId
+                    classBean.schName = dataObject.schName
+                    classBean.schStatus = dataObject.schStatus
+                    classBean.schRemarks = dataObject.schRemarks
+                    classBean.grade = dataObject.parentName
+
+                    classList.push(classBean)
+                }
+            }
+
+            if (classList.length > 0) {
+                classValue.push(classList[0].value)
+                this.setState({
+                    classList,
+                    classValue
+                })
+
+                Toast.loading('获取相册中...', 0)
+                this.getAlbumList(classList[0])
+            }
+        }
+    }
+
     onDeleteVideo = (index) => {
         console.log('删除第' + index)
     }
@@ -100,6 +146,6 @@ export default class WonderMoment extends Component {
     }
 
     handleClassChange = (v) => {
-        this.setState({classText: v})
+        this.setState({classValue: v})
     }
 }

08a341c35c2a5fb651c5ba515a247f2284945959
Arison <784602719@qq.com>
2018-11-29 18:05:32 +0800

    Merge branch 'master' of https://github.com/Arisono/smart-school
    # Conflicts:
    #	src/configs/api.config.js
    #	src/configs/router.config.js

--- src/modules/homework/ReleaseAssignmentPage.js
+++ src/modules/homework/ReleaseAssignmentPage.js
@@ -6,11 +6,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import  './ReleaseAssignmentPage.css'
 import '../../style/css/app-gloal.css'
-import { Input,Button , DatePicker,Icon } from 'antd';
+import { Input,Button , DatePicker,Icon,message } from 'antd';
 import PicturesWallItem from "../../components/upload/PicturesWallItem";
 import TargetSelect from "../../components/TargetSelect";
 import {fetchPost,fetchGet} from '../../utils/fetchRequest';
 import {API} from '../../configs/api.config';
+import {isObjEmpty} from  '../../utils/common';
+
 
 const { TextArea } = Input;
 const teacherData = []
@@ -65,13 +67,24 @@ const targetData = [
  * Created by Arison on 17:47.
  */
 class ReleaseAssignmentPage extends React.Component{
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            name:'ReleaseAssignmentPage',
+        this.state = {
+            name: 'ReleaseAssignmentPage',
             targetList: ['1-1'],
-            targetCount: 1
-        };
+            targetCount: 1,
+            data:{
+                notifyName: '',//标题
+                notifyType: '3',//作业发布
+                notifyDetails: '',//内容
+                notifyCreator: '10000',//创建者
+                notifyStatus: '2',//状态  2发布  1草稿
+                userIds: '10001,10002,10003',//通知
+                notifyFiles: '',
+                startDate: '',//当前时间
+                endDate: null//截止时间
+            }
+        }
     }
 
       componentWillMount(){
@@ -83,31 +96,88 @@ class ReleaseAssignmentPage extends React.Component{
     }
 
     componentDidMount(){
-        fetchPost(API.homeWorkAdd,{
-                      notifyName:'语文课后作业',//标题
-                      notifyType:'3',//作业发布
-                      notifyDetails:'回家背诵唐诗三百首，明天检查',//内容
-                      notifyCreator:'10000',//创建者
-                      notifyStatus:'2',//状态
-                      userIds:'10001,10002,10003'
-                  }).then((response)=>{
-                      console.log("response:"+JSON.stringify(response));
-                  }).catch((error)=>{
-                      console.log("error:"+JSON.stringify(error));
-                  })
-        //获取列表
-        fetchPost(API.homeWorkList,{
-            userId:'10000',
-            notifyType:'3',
-            pageIndex:'1',
-            pageSize:'10'
-        }).then((response)=>{
-            console.log("response:"+JSON.stringify(response));
-        }).catch((error)=>{
-            console.log("error:"+JSON.stringify(error));
+        // fetchGet(API.homeWorkDetail,{
+        //     notifyId:"15"
+        // }).then((response)=>{
+        //     console.log("response:"+JSON.stringify(response));
+        // }).catch((error)=>{
+        //     console.log("error:"+JSON.stringify(error));
+        // })
+    }
+
+
+    changeName=(value)=>{
+                console.log("changeName():"+value.target.value);
+                this.setState({
+                    data:{
+                        notifyName: value.target.value,//标题
+                        notifyType: '3',//作业发布
+                        notifyDetails: this.state.data.notifyDetails,//内容
+                        notifyCreator: '10000',//创建者
+                        notifyStatus: '2',//状态  2发布  1草稿
+                        userIds: '10001,10002,10003',//通知
+                        notifyFiles: '',
+                        startDate: '',//当前时间
+                        endDate: this.state.data.endDate//截止时间
+                    }
+                })
+
+                console.log("changeName():"+JSON.stringify(this.state.targetList));
+    }
+
+    changeContent=(value)=>{
+        console.log("changeName():"+value.target.value);
+        this.setState({
+            data:{
+                notifyDetails: value.target.value,//标题
+                notifyName: this.state.data.notifyName,//标题
+                notifyType: '3',//作业发布
+                notifyCreator: '10000',//创建者
+                notifyStatus: '2',//状态  2发布  1草稿
+                userIds: '10001,10002,10003',//通知
+                notifyFiles: '',
+                startDate: '',//当前时间
+                endDate: this.state.data.endDate//截止时间
+            }
         })
-
-
+    }
+    changeEndDateOk=(value,dateString)=>{
+        console.log("changeName():"+value);
+        console.log("changeName():"+dateString);
+        if(isObjEmpty(dateString)){
+            message.info("请选择日期");
+            return
+        }
+        this.setState({
+            data:{
+                notifyDetails: this.state.data.notifyDetails,//标题
+                notifyName: this.state.data.notifyName,//标题
+                notifyType: '3',//作业发布
+                notifyCreator: '10000',//创建者
+                notifyStatus: '2',//状态  2发布  1草稿
+                userIds: '10001,10002,10003',//通知
+                notifyFiles: '',
+                startDate: '',//当前时间
+                endDate: dateString,//标题
+            }
+        })
+    }
+    changeEndDate=(value,dateString)=>{
+        console.log("changeName():"+value);
+        console.log("changeName():"+dateString);
+        this.setState({
+            data:{
+                notifyDetails: this.state.data.notifyDetails,//标题
+                notifyName: this.state.data.notifyName,//标题
+                notifyType: '3',//作业发布
+                notifyCreator: '10000',//创建者
+                notifyStatus: '2',//状态  2发布  1草稿
+                userIds: '10001,10002,10003',//通知
+                notifyFiles: '',
+                startDate: '',//当前时间
+                endDate: dateString,//标题
+            }
+        })
     }
 
     onTargetChange = (value, label, checkNodes, count) => {
@@ -115,6 +185,40 @@ class ReleaseAssignmentPage extends React.Component{
             targetList: value,
             targetCount: count
         });
+    }
+
+    commitAction=()=>{
+        console.log("commitAction()"+this.state.data.notifyName);
+        console.log("commitAction()"+this.state.data.notifyDetails);
+        console.log("commitAction()"+this.state.data.endDate);
+         if(isObjEmpty(this.state.data.notifyName)){
+              message.info("请输入作业名称");
+             return;
+         }
+        if(isObjEmpty(this.state.data.notifyDetails)){
+            message.info("请输入作业内容");
+            return;
+        }
+        if(isObjEmpty(this.state.data.endDate)){
+            message.info("请输入截止时间");
+            return;
+        }
+        fetchPost(API.homeWorkAdd,{
+            notifyName:this.state.data.notifyName,//标题
+            notifyType:'3',//作业发布
+            notifyDetails:this.state.data.notifyDetails,//内容
+            notifyCreator:'10000',//创建者
+            notifyStatus:'2',//状态
+            endDate:this.state.data.endDate,
+            userIds:'10001,10002,10003'
+        }).then((response)=>{
+            console.log("response:"+JSON.stringify(response));
+            if (response.success){
+                 message.info("发布成功！")
+            }
+        }).catch((error)=>{
+            console.log("error:"+JSON.stringify(error));
+        })
     }
 
     goListAction=()=>{
@@ -149,11 +253,13 @@ class ReleaseAssignmentPage extends React.Component{
                         flex_row_right
                         margin_top_bottom_10
                         margin_left_right_10"  style={{marginRight:"10px"}}>
-                                <DatePicker
+                            <DatePicker
                                 showTime
+                                defaultValue={this.state.data.endDate}
+                                onChange={this.changeEndDate}
                                 format="YYYY-MM-DD HH:mm:ss"
-                                placeholder=""
-                                />
+                                onOk={this.changeEndDateOk}
+                                placeholder=""/>
                         </div>
                     </div>
                 </div>
@@ -162,13 +268,21 @@ class ReleaseAssignmentPage extends React.Component{
             <div className="row" id="row_padding_height">
                 <div className="col-xs-12">
                     <div className="row">
-                    <Input placeholder="请输入作业名称" style={{paddingLeft:"28px",fontSize:"18px"}}  id="input_no_border"/>
+                    <Input placeholder="请输入作业名称"  defaultValue={this.state.data.notifyName}
+                           style={{paddingLeft:"28px",fontSize:"18px"}}
+                           onChange={this.changeName}
+                           id="input_no_border"/>
                     </div>
                     <div className="row">
                         <div  id="page_horizontal_line"></div>
                     </div>
                     <div className="row">
-                        <TextArea rows={4} style={{paddingLeft:"28px",paddingTop:"20px"}} placeholder="请输入作业内容" id="input_no_border"/>
+                        <TextArea rows={4}
+                                  value={this.state.data.notifyDetails}
+                                  onChange={this.changeContent}
+                                  style={{paddingLeft:"28px",paddingTop:"20px"}}
+                                  placeholder="请输入作业内容"
+                                  id="input_no_border"/>
                     </div>
                 </div>
             </div>
@@ -177,11 +291,11 @@ class ReleaseAssignmentPage extends React.Component{
                     <div className="col-xs-12">
                         <div className="row"><div className="col-xs-6" id="page_tile">附件</div></div>
                         <div className="row" id="row_padding_with" >
-                            <PicturesWallItem action={'url路径'} number={1} callback = { this.callback.bind(this)}></PicturesWallItem>
+                            <PicturesWallItem action={API.UPLOAD_FILE} number={1} callback = { this.callback.bind(this)}></PicturesWallItem>
 
                         </div>
                         <div className="row flex_row flex_center margin_top_20" >
-                            <Button type="primary" size="large"  block><span id="span-lager">发 布 作 业</span></Button>
+                            <Button type="primary"  onClick={this.commitAction} size="large"  block><span id="span-lager">发 布 作 业</span></Button>
                         </div>
                         <div id="row_center" onClick={this.goListAction}><span id="link_href" >历史发布</span></div>
                         <div id="bottom_height"></div>
